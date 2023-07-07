6d0d3f9d65e15d1e15cf6e3ef0761a8ff9f24ebd
ktakahiro150397 <k_takahiro_150397@yahoo.co.jp>
2023-07-07 09:49:20 +0900

    単発メッセージ送信

--- discord_send.py
+++ discord_send.py
@@ -1,4 +1,5 @@
-
+import logging
+import requests
 
 
 class discord_send_message():
@@ -7,7 +8,11 @@ class discord_send_message():
     def __init__(self,message:str) -> None:
         self.message = message
 
-
+    def getMessageObject(self) -> dict:
+        '''Returns a dict object representing the message.'''
+        return {
+            "content":self.message
+        }
 
 
 
@@ -16,11 +21,24 @@ class discord_sender():
     '''Send messages to a discord webhook.'''
 
     def __init__(self,webhookUrl:str) -> None:
-        pass
+        self.webhookUrl = webhookUrl
+        
+        self._logger = logging.getLogger(__name__)
+        self._logger.addHandler(logging.NullHandler())
+        self._logger.setLevel(logging.DEBUG)
+        self._logger.propagate = True
 
 
     def sendMessage(self,message:discord_send_message) -> None:
-        pass
+        response = requests.post(
+            url=self.webhookUrl,
+            json=message.getMessageObject(),
+        )
+
+        self._logger.debug(f"Response status code: {response.status_code}")
+        self._logger.debug(f"Response text: {response.text}")
+
+        
 
 
 
